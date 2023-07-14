e3e7c1424f8e8044204bd7de3960065d9f3d54ac
VikingVik123 <vodimmegwa@gmail.com>
2023-07-13 19:10:24 -0700

    Tasks for C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
-/*program that prints the size of various types*/
+/**
+ * main - program that prints the size of various types
+ * Return: 0 Success
+ */
 
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
+char a;
+int b;
+long int c;
+long long int d;
+float f;
 
 printf("Size of a char: 1 byte(s)\n
 Size of an int: 4 byte(s)\n
 Size of a long int: 4 byte(s)\n
 Size of a long long int: 8 byte(s)\n
 Size of a float: 4 byte(s)\n")
-return(0);
+return (0);
 }
