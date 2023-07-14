6e3b042f66fb81d59b6cf493194a281f777afc7a
VikingVik123 <vodimmegwa@gmail.com>
2023-07-13 18:37:09 -0700

    Tasks for C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
 * main - program that prints with puts func
-*return
+*return: 0 (success) 
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
