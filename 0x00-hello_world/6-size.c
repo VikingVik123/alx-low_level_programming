d23f0f2c868309f84bee4cd8bd7148bf7a434caa
VikingVik123 <vodimmegwa@gmail.com>
2023-07-13 19:14:47 -0700

    Tasks for C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ long int c;
 long long int d;
 float f;
 
-printf("Size of a char: 1 byte(s)\n
-Size of an int: 4 byte(s)\n
-Size of a long int: 4 byte(s)\n
-Size of a long long int: 8 byte(s)\n
-Size of a float: 4 byte(s)\n");
+printf("Size of a char: 1 byte(s)\n"
+"Size of an int: 4 byte(s)\n"
+"Size of a long int: 4 byte(s)\n"
+"Size of a long long int: 8 byte(s)\n"
+"Size of a float: 4 byte(s)\n");
 return (0);
 }
