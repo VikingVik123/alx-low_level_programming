95ffdc559e74d151a8b257fbeecc8a03d4f58b23
VikingVik123 <vodimmegwa@gmail.com>
2023-07-14 22:04:02 -0700

    Tasks for C 2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 /**
 *main - Checks for value
 *Return: 0 (Success)
@@ -11,10 +14,9 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%d is positive\n");
-	else if (n==0)
+	else if (n == 0)
 		printf("%d is zero\n");
 	else
 		printf("%d is negative\n")
 	return (0);
 }
-
---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/** 
+*main - Last number check starts 
+*Return: 0 (Success)
+*/
+int main(void)
+{
+	int n;
+	int r;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	r = n % 10;
+	if (r > 5)
+	{	
+		printf("Last digit %d is %d and is greater than 5\n",n ,r);
+	}
+	else if (r==0)
+	{
+		printf("Last digit of %d is %d and is 0\n",n ,r);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and less than 6 and is not 0\n",n ,r);
+	return (0);
+}
