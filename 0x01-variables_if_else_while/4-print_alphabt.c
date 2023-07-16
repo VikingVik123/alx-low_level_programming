052f0a97a97f748dd392d480cf35fa9df2831806
VikingVik123 <vodimmegwa@gmail.com>
2023-07-15 18:41:41 -0700

    Tasks for C 2

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - Print Alphabets
+*Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{	n++
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
