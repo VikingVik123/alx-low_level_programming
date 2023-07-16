63703a71d6d35fd3fd0bef4e5e470535b2e6831b
VikingVik123 <vodimmegwa@gmail.com>
2023-07-15 19:25:29 -0700

    Tasks for C 2

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - Print Alphabets
+*Return: 0 (Success)
+*/
+
+int main(void)
+{
+
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + 48);
+	}
+	printf("\n");
+	return (0);
+}
