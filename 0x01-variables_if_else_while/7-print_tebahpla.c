c8fd4002021becc579d833adfacccf4eb2c4ab8a
VikingVik123 <vodimmegwa@gmail.com>
2023-07-15 19:48:57 -0700

    Tasks for C 2

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - Print Alphabets
+*Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n')
+	return (0);
+}
