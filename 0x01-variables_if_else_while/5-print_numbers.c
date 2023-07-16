c3065d3a0fa0ca0169963a27e85d7807790f007e
VikingVik123 <vodimmegwa@gmail.com>
2023-07-15 19:13:13 -0700

    Tasks for C 2

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - Print Alphabets
+*Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
