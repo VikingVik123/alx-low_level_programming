1bcffb38e646a871bd11d886078a988c5bcb244a
VikingVik123 <vodimmegwa@gmail.com>
2023-07-19 01:27:42 -0700

    Tasks for nested loop

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
 *main - Entry point
-*Description: Print Alphabet
+*Description: print_alphabet:prints letters from a - z
 *Return: 0 success
 */
 void print_alphabet(void)
 {
 	int n;
+
 	for (n = 97; n <= 122; n++)
 	{
 		_putchar(n);
