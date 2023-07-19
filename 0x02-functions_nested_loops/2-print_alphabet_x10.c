b8b0dc1b775af3e3d9b303333be76fdac386938b
VikingVik123 <vodimmegwa@gmail.com>
2023-07-19 06:33:21 -0700

    Task for nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *print_alphabet_x10: Entry point
- *Description: Prints a-z x10
+ *Description:print_alphabet_x10 Prints a-z x10
  * Return: Always 0
  */
 void print_alphabet_x10(void)
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	int n;
 	int j;
 
-	for (j = 1; n <= 10; n++)
+	for (n = 1; n <= 10; n++)
 	{
 		for (j = 97; j <= 122; j++)
 		{
