8a0c627e7a6cb77a12aeabb6519cef98d8d2b320
VikingVik123 <vodimmegwa@gmail.com>
2023-07-24 09:09:46 -0700

    Tasks for 0x05

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - checks string length
+ * @s: string
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
