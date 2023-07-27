4d7015394d68f011bede652874bce08c0efe070c
VikingVik123 <vodimmegwa@gmail.com>
2023-07-27 08:58:59 -0700

    Task 0 in 0x06

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+*_strcat(char *dest, char *src) - function to combine strings
+*@src :string to add
+*@dest : character to be attached to
+*return :combined string if successful
+*/
+
+char *_strcat(char *dest, char *src)
+
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
