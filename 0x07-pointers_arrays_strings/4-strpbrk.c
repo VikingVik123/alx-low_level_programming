ef6ffb552c53dacd31f5ace01ee13e0e2f6c25a3
VikingVik123 <vodimmegwa@gmail.com>
2023-07-31 19:47:22 -0700

    Tasks for 0x07

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,18 +7,20 @@
  * Return: 0 if Successful
  */
 
+
 char *_strpbrk(char *s, char *accept)
 {
-	int i, n;
+	int k;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
-		for (n = 0; accept[n] != '\0'; n++)
+		for (k = 0; accept[k]; k++)
 		{
-			if (s[i] == accept[n])
-				return (s + i);
+		if (*s == accept[k])
+		return (s);
 		}
+	s++;
 	}
 
-	return (NULL);
+return ('\0');
 }
