ad28775b1fb18b35dd47dcbee9dbb7653b7b0ef6
VikingVik123 <vodimmegwa@gmail.com>
2023-08-03 08:34:55 -0700

    Tasks for 0x08

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,15 +8,11 @@
  */
 int factorial(int n)
 {
-	if (n <= 0)
-	{
+	if (n < 0)
 		return (-1);
-	}
+
+	if (n <= 1)
+		return (1);
+
 	return (n * factorial(n - 1));
-}
-int main(void)
-{
-	int n;
-	printf("%d\n", n);
-	return (0);
 }
