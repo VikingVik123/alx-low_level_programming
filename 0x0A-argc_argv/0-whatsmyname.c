174c6740697fd2aba511a40097786b9be6f15a41
VikingVik123 <vodimmegwa@gmail.com>
2023-08-09 05:17:56 -0700

    Task 0 for 0x0A

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Entry point
+ *@argc:no of args
+ *@argv:array of args
+ *Return: name of program
+ */
+int main (int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This directory contains the tasks for 0x0A-argc_argv
