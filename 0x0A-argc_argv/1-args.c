0dd844b1301e174a13c7a00bc1ac56bc00da5012
VikingVik123 <vodimmegwa@gmail.com>
2023-08-09 06:37:06 -0700

    Task 1 for 0x0A

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ *@argc: int arg
+ *@argv: array arg
+ *Return: 0 if successful
+ */
+
+int main(int argc, char *argv[])
+{
+        printf("%d\n", argc - 1);
+        return (0);
+}
