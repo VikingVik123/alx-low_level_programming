8895432ea7e64f2dcf03346cd7267d88d6606b05
VikingVik123 <vodimmegwa@gmail.com>
2023-08-09 07:45:01 -0700

    Task 3 for 0x0A

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ */
+
+int main(int argc, char *argv[0])
+{
+	int coun;
+
+	printf("%s\n", argv[coun]);
+	return (0);
+}
