3ba898a2cf3bc57fd04fbaf9f0f59b7ef747850f
VikingVik123 <vodimmegwa@gmail.com>
2023-08-14 03:54:59 -0700

    Task for 1-pi.h

---
+++ 0x0D-preprocessor/m.c
@@ -0,0 +1,11 @@
+#include "try.h"
+#include <stdio.h>
+
+int main()
+{
+        int s = (2 * PI);
+        {
+		printf("%d\n", s);
+		return(0);
+        }
+}
---
+++ 0x0D-preprocessor/try2.c
@@ -0,0 +1,8 @@
+#include "0-object_like_macro.h"
+#include <stdio.h>
+int main()
+{
+	printf("%d\n", SIZE);
+	return (0);
+}
+
