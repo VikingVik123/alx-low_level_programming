21133af49a6dfddeaaaaf5b6467bbb0151493a99
VikingVik123 <vodimmegwa@gmail.com>
2023-08-16 09:27:07 -0700

    Task 1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
-/**
- * print_name - prints a name
- * @name: name 2 print
- * @f: pointer to function
- */
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
-		return;
-	f(name);
-}
+	if (name || *f)
+	{
+
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (!array || !action)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
