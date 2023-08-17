c02d492471ca536fe09dc504e0487f3f1444f211
VikingVik123 <vodimmegwa@gmail.com>
2023-08-17 03:06:53 -0700

    Tasks

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "function_pointers.h"
-
+/**
+ * print_name - Entry point
+ * @name: pointer to name variable
+ * @f:pointer
+ * Return: char stored in pointer 
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name || *f)
