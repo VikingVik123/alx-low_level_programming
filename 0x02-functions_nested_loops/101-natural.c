95bb929038afc4ceacb6f0307ffb90755ede8c55
VikingVik123 <vodimmegwa@gmail.com>
2023-08-23 00:21:06 -0700

    Corrections

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - computes and prints the sum of all the multiples
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	unsigned long int sum1, sum2, sum;
+	int i;
+
+	sum1 = 0;
+	sum2 = 0;
+	sum = 0;
+
+	for (i = 0; i < 1024; ++i)
+	{
+		if ((i % 3) == 0)
+		{
+			sum1 = sum1 + i;
+		} else if ((i % 5) == 0)
+		{
+			sum2 = sum2 + i;
+		}
+	}
+	sum = sum1 + sum2;
+	printf("%lu\n", sum);
+	return (0);
+}
