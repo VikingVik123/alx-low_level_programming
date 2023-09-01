99d21185aafe7de8344b4432c54e1b19c55736fa
VikingVik123 <vodimmegwa@gmail.com>
2023-08-31 20:11:05 -0700

    Header file and task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,10 +11,10 @@ unsigned int binary_to_uint(const char *b)
 	int x;
 	unsigned int dec = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
-	for (x = 0; b[x] != '\0'; x++)
+	for (x = 0; b[x]; x++)
 	{
 		if (b[x] < '0' || b[x] > '1')
 			return (0);
