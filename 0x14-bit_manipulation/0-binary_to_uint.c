de6bd9b252effbc7b0f4ec69e8177729a21b8e09
VikingVik123 <vodimmegwa@gmail.com>
2023-08-31 20:04:16 -0700

    header file and task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/*
+ * unsigned int binary_to_uint(const char *b) - converts binary to decimal
+ * @b : pointer to string of char
+ * Return: converted number or 0
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int x;
+	unsigned int dec = 0;
+
+	if (b == NULL)
+		return (0);
+
+	for (x = 0; b[x] != '\0'; x++)
+	{
+		if (b[x] < '0' || b[x] > '1')
+			return (0);
+		dec = 2 * dec + (b[x] - '0');
+	}
+	return (dec);
+}
