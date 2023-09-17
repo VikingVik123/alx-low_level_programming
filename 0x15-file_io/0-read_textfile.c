2f61d04d708303f5b308d46c2cb911d509878d21
VikingVik123 <vodimmegwa@gmail.com>
2023-09-17 00:28:03 -0700

    Late correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,19 +9,19 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer;
-	ssize_t o;
+	char *buf;
+	ssize_t fd;
 	ssize_t w;
 	ssize_t t;
 
-	o = open(filename, O_RDONLY);
-	if (o == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-	buffer = malloc(sizeof(char) * letters);
+	buf = malloc(sizeof(char) * letters);
 	t = read(fd, buf, letters);
 	w = write(STDOUT_FILENO, buf, t);
 
-	free(buffer);
-	close(o);
+	free(buf);
+	close(fd);
 	return (w);
 }
