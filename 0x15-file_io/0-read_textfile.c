785ed2021f2b3aa5d72088de64157e83efe30960
VikingVik123 <vodimmegwa@gmail.com>
2023-10-02 17:59:24 -0700

    Retry

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,7 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
 	ssize_t fd;
-	ssize_t w;
+	ssize_t r;
 	ssize_t t;
 
 	fd = open(filename, O_RDONLY);
@@ -21,10 +21,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	buf = malloc(sizeof(char) * letters);
 
+	r = write(STDOUT_FILENO, buf, t);
 	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
 	free(buf);
 	close(fd);
 
-	return (w);
+	return (r);
 }
