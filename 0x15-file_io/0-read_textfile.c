80b187fce2c66528570c7c05caf6a5575dde3697
VikingVik123 <vodimmegwa@gmail.com>
2023-10-02 17:54:57 -0700

    Retry

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,27 +4,27 @@
 * read_textfile- Read text file print to STDOUT.
 * @filename: text file being read
 * @letters: number of letters to be read
-* Return: r = actual number of bytes read and printed
+* Return: w = actual number of bytes read and printed
 */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer;
+	char *buf;
 	ssize_t fd;
-	ssize_t r;
+	ssize_t w;
 	ssize_t t;
 
 	fd = open(filename, O_RDONLY);
-	t = read(fd, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, t);
 
 	if (fd == -1)
 		return (0);
 
 	buf = malloc(sizeof(char) * letters);
 
-	free(buffer);
-	fclose(fd);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+	free(buf);
+	close(fd);
 
-	return (r);
+	return (w);
 }
