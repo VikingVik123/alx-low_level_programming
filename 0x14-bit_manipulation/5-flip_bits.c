4cbddd7131c23032499d365b7c3c2f58504acd9b
VikingVik123 <vodimmegwa@gmail.com>
2023-10-26 12:07:52 -0700

    Trial

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ *
+ * * flip_bits - flip bits to convert one number to another number
+ *
+ * * @n: first number
+ *
+ * * @m: second number
+ *
+ * * Return: number of bits to flip
+ *
+ * */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+
+{
+
+	unsigned long int d;
+
+	int counter;
+
+	d = n ^ m;
+
+	counter = 0;
+
+	while (d)
+
+	{
+
+		counter++;
+
+		d &= (d - 1);
+
+	}
+
+	return (counter);
+
+}
